2481c0f6c71412e9f4161827c79c06768232cdf7
superfliege <sypherify@live.de>
2025-01-31 22:13:36 +0100

    Add command-line arguments for MIDI device configuration and control mode

--- midi.py
+++ midi.py
@@ -2,18 +2,30 @@ import mido
 from mido import MidiFile, MidiTrack, Message
 import math
 import threading
+import argparse
 
 #### CONFIGURATION ####
 # if you have only one device connected, leave inputname empty
 # if you have multiple devices connected, specify the name of the device you want to use
-# If you want to use the LPD8 to change the midi channel, set lpd8controlMode to True
-inputnamesearchname = "KOMPLETE"
-inputnamesearchname2 = "MPK mini"
-inputnamethrusearchname = "LPD8"
-outputsearchname = "U2MIDI"
-lpd8controlMode = True
-midi_max_value = 80
-midi_exponent = 0.60
+# If you want to use the LPD8 to change the midi channel, set midichannelcontrolmode to True
+parser = argparse.ArgumentParser(description='MIDI Velocity Curve Changer')
+parser.add_argument('--inputmididevicename', type=str, default="KOMPLETE", help='Search name for input device')
+parser.add_argument('--inputmididevicename2', type=str, default="MPK mini", help='Second search name for input device')
+parser.add_argument('--inputmididevicenamemidichannel', type=str, default="LPD8", help='Search name for input device to change midi channel')
+parser.add_argument('--outputmididevicename', type=str, default="U2MIDI", help='Search name for output device')
+parser.add_argument('--midichannelcontrolmode', type=bool, default=True, help='Enable LPD8 control mode')
+parser.add_argument('--midi_max_value', type=int, default=80, help='Maximum MIDI value for velocity adjustment')
+parser.add_argument('--midi_exponent', type=float, default=0.60, help='Exponent for velocity adjustment')
+
+args = parser.parse_args()
+
+inputmididevicename = args.inputmididevicename
+inputmididevicename2 = args.inputmididevicename2
+inputmididevicenamemidichannel = args.inputmididevicenamemidichannel
+outputmididevicename = args.outputmididevicename
+midichannelcontrolmode = args.midichannelcontrolmode
+midi_max_value = args.midi_max_value
+midi_exponent = args.midi_exponent
 #### END CONFIGURATION ####
 
 inputname  = ""
@@ -25,16 +37,16 @@ channelchange = 1
 print("Available input ports:")
 for port in mido.get_input_names():
     print(port)
-    if inputnamesearchname in port or inputnamesearchname2 in port:
+    if inputmididevicename in port or inputmididevicename2 in port:
         print("Using port for INPUT. ", port)
         inputname = port
 
-if lpd8controlMode == True:
+if midichannelcontrolmode == True:
     print("Available input ports:")
     for port in mido.get_input_names():
         print(port)
-        if "MPK mini" in port or "LPD8" in port:
-            print("Using port for INPUTTHRU. ", port)
+        if inputmididevicenamemidichannel in port:
+            print("Using port for change midi channel:", port)
             inputnamethru = port
 
 
@@ -42,7 +54,7 @@ if lpd8controlMode == True:
 print("\nAvailable output ports:")
 for port in mido.get_output_names():
     print(port)
-    if "U2MIDI" in port:
+    if outputmididevicename in port:
         print("Using port for OUTPUT: ", port)
         outputname = port
 
@@ -92,6 +104,6 @@ def midithruthread():
 
 thread = threading.Thread(target=midithread)
 thread.start()
-if lpd8controlMode == True:
+if midichannelcontrolmode == True:
     thread2 = threading.Thread(target=midithruthread)
     thread2.start()
--- start-midi.sh
+++ start-midi.sh
@@ -1,1 +1,1 @@
-python3 midi.py
+python3 midi.py --inputnamesearchname "KOMPLETE" --inputnamesearchname2 "MPK mini" --inputnamethrusearchname "LPD8" --outputsearchname "U2MIDI" --lpd8controlMode True --midi_max_value 80 --midi_exponent 0.60
