c1ae2e3ea42bd2d5355f96164c16b26efc6e72a7
superfliege <sypherify@live.de>
2025-02-01 00:01:11 +0100

    Remove commented debug mode lines from startup script

--- start-midi.sh
+++ start-midi.sh
@@ -1,8 +1,6 @@
 # Minimum Startup Script
 #python3 midi.py --inputmididevicename "KOMPLETE" --outputmididevicename "U2MIDI" --midi_max_value 80 --midi_exponent 0.60
-
 # Minimum Startup Script with debug mode
 #python3 midi.py --inputmididevicename "KOMPLETE" --outputmididevicename "U2MIDI" --midi_max_value 80 --midi_exponent 0.60 --debugmode 1
-
 # Full Startup Script
 python3 midi.py --inputmididevicename "KOMPLETE" --outputmididevicename "U2MIDI" --inputmididevicenamechannelcontrol "LPD8" --midi_max_value 80 --midi_exponent 0.60
