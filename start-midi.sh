49e97fb62e08611c6ddc65d44769828f4307431d
superfliege <sypherify@live.de>
2025-01-31 23:23:07 +0100

    Update start-midi.sh with improved startup script examples and corrected input device name

--- start-midi.sh
+++ start-midi.sh
@@ -1,1 +1,4 @@
-python3 midi.py --inputmididevicename "KOMPLETE" --inputmididevicename2 "MPK mini" --inputmididevicenamemidichannel "LPD8" --outputmididevicename "U2MIDI" --midi_max_value 80 --midi_exponent 0.60
+# Minimum Startup Script
+#python3 midi.py --inputmididevicename "KOMPLETE" --outputmididevicename "U2MIDI" --midi_max_value 80 --midi_exponent 0.60
+# Full Startup Script
+python3 midi.py --inputmididevicename "KOMPLETE" --outputmididevicename "U2MIDI" --inputmididevicenamechannelcontrol "LPD8" --midi_max_value 80 --midi_exponent 0.60
